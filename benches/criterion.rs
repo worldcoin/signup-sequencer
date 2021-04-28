2182005e1307f3938e8e257384692798a7d6f207
Remco Bloemen <remco@0x.org>
2021-04-28 16:43:39 -0700

    Remove criterion hack

--- benches/criterion.rs
+++
@@ -1,8 +0,0 @@
-use criterion::Criterion;
-use rust_app_template::bench;
-
-fn main() {
-    let mut criterion = Criterion::default().configure_from_args();
-    bench::main(&mut criterion);
-    criterion.final_summary();
-}

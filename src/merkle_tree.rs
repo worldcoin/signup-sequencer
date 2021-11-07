977f18c57076d927d1b7a0dd42511aeca035d62e
Remco Bloemen <remco@wicked.ventures>
2021-11-07 09:43:14 -0800

    Implement range set

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -2,12 +2,11 @@
 //!
 //! # To do
 //!
-//! * Batch set support
 //! * Disk based storage backend (using mmaped files should be easy)
 
 use std::{
     fmt::Debug,
-    iter::{repeat, successors},
+    iter::{once, repeat, successors},
 };
 
 /// Hash types, values and algorithms for a Merkle tree
@@ -49,6 +48,25 @@ pub enum Branch<H: Hasher> {
 #[derive(Clone, PartialEq, Eq)]
 pub struct Proof<H: Hasher>(Vec<Branch<H>>);
 
+/// For a given node index, return the parent node index
+/// Returns None if there is no parent (root node)
+const fn parent(index: usize) -> Option<usize> {
+    if index == 0 {
+        None
+    } else {
+        Some(((index + 1) >> 1) - 1)
+    }
+}
+
+/// For a given node index, return index of the first (left) child.
+const fn first_child(index: usize) -> usize {
+    (index << 1) + 1
+}
+
+const fn depth(index: usize) -> usize {
+    63 - (index + 1).leading_zeros() as usize
+}
+
 impl<H: Hasher> MerkleTree<H> {
     pub fn new(depth: usize) -> Self {
         // Compute empty node values, leaf to root
@@ -88,40 +106,44 @@ impl<H: Hasher> MerkleTree<H> {
     }
 
     pub fn set(&mut self, leaf: usize, hash: H::Hash) {
-        // Update leaf
-        assert!(leaf < self.num_leaves());
-        let mut index = self.num_leaves() + leaf - 1;
-        self.nodes[index] = hash;
+        self.set_range(leaf, once(hash));
+    }
 
-        // Update tree nodes
-        while index != 0 {
-            // Map index to parent index
-            index = ((index + 1) >> 1) - 1;
-
-            // Recompute node hash
-            let child = (index << 1) + 1; // Left child, right is +1
-            self.nodes[index] = H::hash_node(&self.nodes[child], &self.nodes[child + 1]);
+    pub fn set_range<I: IntoIterator<Item = H::Hash>>(&mut self, start: usize, hashes: I) {
+        let index = self.num_leaves() + start - 1;
+        let mut count = 0;
+        // TODO: Error/panic when hashes is longer than available leafs
+        for (leaf, hash) in self.nodes[index..].iter_mut().zip(hashes) {
+            *leaf = hash;
+            count += 1;
+        }
+        if count != 0 {
+            self.update_nodes(index, index + (count - 1));
         }
     }
 
-    // TODO: Batch set
-    // pub fn set_batch<I: IntoIterator<Item = H::Hash>>(&mut self, offset: usize,
-    // hashes: I) {     todo!()
-    // }
+    fn update_nodes(&mut self, start: usize, end: usize) {
+        debug_assert_eq!(depth(start), depth(end));
+        if let (Some(start), Some(end)) = (parent(start), parent(end)) {
+            for parent in start..=end {
+                let child = first_child(parent);
+                self.nodes[parent] = H::hash_node(&self.nodes[child], &self.nodes[child + 1]);
+            }
+            self.update_nodes(start, end);
+        }
+    }
 
     pub fn proof(&self, leaf: usize) -> Proof<H> {
         let mut index = self.num_leaves() + leaf - 1;
         let mut path = Vec::with_capacity(self.depth);
-        while index != 0 {
+        while let Some(parent) = parent(index) {
             // Add proof for node at index to parent
             path.push(match index & 1 {
                 1 => Branch::Left(self.nodes[index + 1].clone()),
                 0 => Branch::Right(self.nodes[index - 1].clone()),
                 _ => unreachable!(),
             });
-
-            // Map index to parent index
-            index = ((index + 1) >> 1) - 1;
+            index = parent;
         }
         Proof(path)
     }
@@ -197,6 +219,24 @@ pub mod test {
         fn hash_node(left: &Self::Hash, right: &Self::Hash) -> Self::Hash {
             keccak256([*left, *right].concat())
         }
+    }
+
+    #[test]
+    fn test_index_calculus() {
+        assert_eq!(parent(0), None);
+        assert_eq!(parent(1), Some(0));
+        assert_eq!(parent(2), Some(0));
+        assert_eq!(parent(3), Some(1));
+        assert_eq!(parent(4), Some(1));
+        assert_eq!(parent(5), Some(2));
+        assert_eq!(parent(6), Some(2));
+        assert_eq!(first_child(0), 1);
+        assert_eq!(first_child(2), 5);
+        assert_eq!(depth(0), 0);
+        assert_eq!(depth(1), 1);
+        assert_eq!(depth(2), 1);
+        assert_eq!(depth(3), 2);
+        assert_eq!(depth(6), 2);
     }
 
     #[test]

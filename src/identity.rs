a93d7994f0e67bdc2d50bda25baf6949a23f2872
lucasege <lucas.ege@worldcoin.org>
2021-11-12 13:23:06 -0800

    Address reviews

--- src/identity.rs
+++ src/identity.rs
@@ -3,31 +3,36 @@ use crate::{
     solidity::{ContractSigner, SemaphoreContract},
 };
 use ethers::prelude::Middleware;
-use eyre::{bail, Error as EyreError};
-use std::{convert::TryInto, sync::Arc};
+use eyre::{bail, Error as EyreError, Result as EyreResult};
+use std::convert::TryInto;
 
 pub type Commitment = Hash;
 
 pub fn inclusion_proof_helper(tree: &MimcTree, commitment: &str) -> Result<Proof, EyreError> {
-    let decoded_commitment = hex::decode(commitment).unwrap();
-    let decoded_commitment: Commitment = (&decoded_commitment[..]).try_into().unwrap();
+    let decoded_commitment = hex::decode(commitment)?;
+    let decoded_commitment: Commitment = (&decoded_commitment[..]).try_into()?;
     if let Some(index) = tree.position(&decoded_commitment) {
         return Ok(tree.proof(index));
     }
     bail!("Commitment not found {}", commitment);
 }
 
-pub fn insert_identity_commitment(tree: &mut MimcTree, commitment: &str, index: usize) {
-    let decoded_commitment = hex::decode(commitment).unwrap();
-    let commitment: Commitment = (&decoded_commitment[..]).try_into().unwrap();
+pub fn insert_identity_commitment(
+    tree: &mut MimcTree,
+    commitment: &str,
+    index: usize,
+) -> EyreResult<()> {
+    let decoded_commitment = hex::decode(commitment)?;
+    let commitment: Commitment = (&decoded_commitment[..]).try_into()?;
     tree.set(index, commitment);
+    Ok(())
 }
 
 pub async fn insert_identity_to_contract(
     semaphore_contract: &SemaphoreContract,
-    signer: Arc<ContractSigner>,
+    signer: &ContractSigner,
     commitment: &str,
-) -> Result<bool, EyreError> {
+) -> EyreResult<bool> {
     let decoded_commitment = hex::decode(commitment).unwrap();
     let tx = semaphore_contract.insert_identity(decoded_commitment[..].into());
     let pending_tx = signer.send_transaction(tx.tx, None).await.unwrap();
--- src/solidity.rs
+++ src/solidity.rs
@@ -1,11 +1,13 @@
-use std::sync::Arc;
-
 use ethers::{
     core::k256::ecdsa::SigningKey,
     prelude::{
         abigen, Address, Http, LocalWallet, Middleware, Provider, Signer, SignerMiddleware, Wallet,
+        H160,
     },
 };
+use eyre::eyre;
+use hex_literal::hex;
+use std::sync::Arc;
 
 abigen!(
     Semaphore,
@@ -15,25 +17,25 @@ abigen!(
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
-const SEMAPHORE_ADDRESS: &str = "0xFE600E2C8023d28219F65C5ED2dDED310737742a";
+const SEMAPHORE_ADDRESS: Address = H160(hex!("FE600E2C8023d28219F65C5ED2dDED310737742a"));
+const SIGNING_KEY: [u8; 32] =
+    hex!("ee79b5f6e221356af78cf4c36f4f7885a11b67dfcc81c34d80249947330c0f82");
 
 pub type ContractSigner = SignerMiddleware<Provider<Http>, Wallet<SigningKey>>;
 pub type SemaphoreContract = Semaphore<ContractSigner>;
 
-pub async fn initialize_semaphore() -> Result<(Arc<ContractSigner>, SemaphoreContract), eyre::Error>
-{
+pub async fn initialize_semaphore() -> Result<(ContractSigner, SemaphoreContract), eyre::Error> {
     let provider = Provider::<Http>::try_from("http://localhost:8545")
         .expect("could not instantiate HTTP Provider");
-    let chain_id = provider.get_chainid().await.unwrap();
+    let chain_id: u64 = provider
+        .get_chainid()
+        .await?
+        .try_into()
+        .map_err(|e| eyre!("{}", e))?;
 
-    let wallet = "ee79b5f6e221356af78cf4c36f4f7885a11b67dfcc81c34d80249947330c0f82"
-        .parse::<LocalWallet>()?;
-    let wallet = wallet.with_chain_id(chain_id.as_u64());
+    let wallet = LocalWallet::from(SigningKey::from_bytes(&SIGNING_KEY)?).with_chain_id(chain_id);
+    let signer = SignerMiddleware::new(provider, wallet);
+    let contract = Semaphore::new(SEMAPHORE_ADDRESS, Arc::new(signer.clone()));
 
-    let signer = SignerMiddleware::new(provider.clone(), wallet);
-    let signer = Arc::new(signer);
-
-    let semaphore_address = SEMAPHORE_ADDRESS.parse::<Address>().unwrap();
-    let contract = Semaphore::new(semaphore_address, signer.clone());
     Ok((signer, contract))
 }

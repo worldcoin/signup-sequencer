482d38a9d21f39f8b36e4288a07fcbe287708b96
psippl <philsippl@gmail.com>
2022-04-22 17:01:37 +0200

    adapt abi

--- src/ethereum/contract.rs
+++ src/ethereum/contract.rs
@@ -5,8 +5,8 @@ abigen!(
     r#"[
         event MemberAdded(uint256 indexed groupId, uint256 identityCommitment, uint256 leaf)
         function getDepth(uint256 groupId) public view returns (uint8)
-        function createAirdrop(uint256 airdropId, address coordinator, uint8 depth) public override
-        function addRecipient(uint256 airdropId, uint256 identityCommitment) public override
+        function createGroup(uint256 groupId, uint8 depth, uint256 zeroValue) public override
+        function addMember(uint256 groupId, uint256 identityCommitment) public override
     ]"#,
     event_derives(serde::Deserialize, serde::Serialize)
 );

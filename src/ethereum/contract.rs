bbffb026266a90abdda9bcb38188b22f85d849b7
Philipp Sippl <philsippl@gmail.com>
2022-05-02 14:41:33 +0200

    fix event and remove tx check (#121)

--- src/ethereum/contract.rs
+++ src/ethereum/contract.rs
@@ -3,7 +3,7 @@ use ethers::contract::abigen;
 abigen!(
     SemaphoreAirdrop,
     r#"[
-        event MemberAdded(uint256 indexed groupId, uint256 identityCommitment, uint256 leaf)
+        event MemberAdded(uint256 indexed groupId, uint256 identityCommitment, uint256 root)
         function manager() public view returns (address)
         function getDepth(uint256 groupId) public view returns (uint8)
         function createGroup(uint256 groupId, uint8 depth, uint256 zeroValue) public override

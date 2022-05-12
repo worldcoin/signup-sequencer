e3a26e168490ab66a7d8f815ca4accd853025790
Remco Bloemen <remco@wicked.ventures>
2022-05-12 16:16:55 -0700

    Gas oracle

--- src/ethereum/mod.rs
+++ src/ethereum/mod.rs
@@ -11,15 +11,19 @@ use chrono::{Duration as ChronoDuration, Utc};
 use ethers::{
     core::k256::ecdsa::SigningKey,
     middleware::{
-        gas_oracle::{Cache, GasOracleMiddleware, Polygon},
+        gas_oracle::{
+            Cache, EthGasStation, Etherchain, GasNow, GasOracle, GasOracleMiddleware, Median,
+            Polygon, ProviderOracle,
+        },
         NonceManagerMiddleware, SignerMiddleware,
     },
     providers::{Middleware, Provider},
     signers::{LocalWallet, Signer, Wallet},
-    types::{Address, BlockId, BlockNumber, Chain, H160, H256, U256},
+    types::{BlockId, BlockNumber, Chain, H160, H256},
 };
 use eyre::{eyre, Result as EyreResult};
-use futures::try_join;
+use futures::{try_join, FutureExt};
+use reqwest::Client as ReqwestClient;
 use std::{sync::Arc, time::Duration};
 use structopt::StructOpt;
 use tracing::{error, info, instrument};
@@ -53,15 +57,13 @@ pub struct Options {
     pub eip1559: bool,
 }
 
-type GasOracle = Cache<GasOracleLogger<Polygon>>;
-
 // Code out the provider stack in types
 // Needed because of <https://github.com/gakonst/ethers-rs/issues/592>
 type Provider0 = Provider<RpcLogger<Transport>>;
 type Provider1 = SignerMiddleware<Provider0, Wallet<SigningKey>>;
 type Provider2 = NonceManagerMiddleware<Provider1>;
 type Provider3 = Estimator<Provider2>;
-type Provider4 = GasOracleMiddleware<Provider3, GasOracle>;
+type Provider4 = GasOracleMiddleware<Arc<Provider3>, Box<dyn GasOracle>>;
 pub type ProviderStack = Provider4;
 
 #[derive(Clone, Debug)]
@@ -80,7 +82,7 @@ impl Ethereum {
         // TODO: Does the WebSocket impl handle dropped connections by
         // reconnecting? What is the timeout on stalled connections? What is
         // the retry policy?
-        let (provider, chain_id) = {
+        let (provider, chain_id, eip1559) = {
             info!(
                 provider = %&options.ethereum_provider,
                 "Connecting to Ethereum"
@@ -90,9 +92,13 @@ impl Ethereum {
             let provider = Provider::new(logger);
 
             // Fetch state of the chain.
-            let (chain_id, latest_block) = try_join!(
+            let (version, chain_id, latest_block, eip1559) = try_join!(
+                provider.client_version(),
                 provider.get_chainid(),
-                provider.get_block(BlockId::Number(BlockNumber::Latest))
+                provider.get_block(BlockId::Number(BlockNumber::Latest)),
+                provider
+                    .fee_history(1, BlockNumber::Latest, &[])
+                    .map(|r| Ok(r.is_ok()))
             )?;
 
             // Identify chain.
@@ -109,7 +115,7 @@ impl Ethereum {
                 .number
                 .ok_or_else(|| eyre!("Could not read latest block number"))?;
             let block_time = latest_block.time()?;
-            info!(%chain_id, %chain, %block_number, ?block_hash, %block_time, "Connected to Ethereum provider");
+            info!(%version, %chain_id, %chain, %eip1559, %block_number, ?block_hash, %block_time, "Connected to Ethereum provider");
 
             // Sanity check the block timestamp
             let now = Utc::now();
@@ -123,7 +129,7 @@ impl Ethereum {
                 // Log an error, but proceed anyway since this doesn't technically block us.
                 error!(%now, %block_time, %block_age, "Block time is more than 30 minutes from now.");
             }
-            (provider, chain_id)
+            (provider, chain_id, eip1559)
         };
 
         // Construct a local key signer
@@ -159,17 +165,48 @@ impl Ethereum {
         // TODO: Check signer balance regularly and keep the metric as a gauge.
 
         // Add a gas estimator with 10% and 10k gas bonus over provider.
+        // TODO: Use local EVM evaluation?
         let provider = Estimator::new(provider, 1.10, 10e3);
 
         // Add a gas oracle.
         let provider = {
-            let client = reqwest::Client::builder()
-                .timeout(Duration::from_secs(30))
-                .build()?;
-            let chain = Chain::try_from(chain_id)?;
-            let gas_oracle = Polygon::with_client(client, chain)?;
-            let gas_oracle = GasOracleLogger::new(gas_oracle);
-            let gas_oracle = Cache::new(Duration::from_secs(5), gas_oracle);
+            // Start with a medianizer
+            let mut median = Median::new();
+
+            // Construct a fallback oracle
+            let provider = Arc::new(provider);
+            median.add_weighted(0.1, ProviderOracle::new(provider.clone()));
+
+            // Utility to get a Reqwest client with 30s timeout.
+            let client = || -> EyreResult<ReqwestClient> {
+                ReqwestClient::builder()
+                    .timeout(Duration::from_secs(30))
+                    .build()
+                    .map_err(|err| err.into())
+            };
+
+            // Add more oracles to the median based on the chain we are on.
+            if let Ok(chain) = Chain::try_from(chain_id) {
+                match chain {
+                    Chain::Mainnet => {
+                        let client = client()?;
+                        median.add(EthGasStation::with_client(client.clone(), None));
+                        median.add(Etherchain::with_client(client.clone()));
+                        median.add(GasNow::with_client(client));
+                    }
+                    Chain::Polygon | Chain::PolygonMumbai => {
+                        median.add(Polygon::with_client(client()?, chain)?);
+                    }
+                    _ => {}
+                }
+            }
+
+            // Add a logging, caching and abstract the type.
+            let logger = GasOracleLogger::new(median);
+            let cache = Cache::new(Duration::from_secs(5), logger);
+            let gas_oracle: Box<dyn GasOracle> = Box::new(cache);
+
+            // Wrap in a middleware
             GasOracleMiddleware::new(provider, gas_oracle)
         };
 
@@ -177,48 +214,13 @@ impl Ethereum {
         Ok(Self { provider, address })
     }
 
-    pub fn provider(&self) -> &Arc<ProviderStack> {
+    #[must_use]
+    pub const fn provider(&self) -> &Arc<ProviderStack> {
         &self.provider
     }
 
-    pub fn address(&self) -> H160 {
+    #[must_use]
+    pub const fn address(&self) -> H160 {
         self.address
-    }
-
-    pub async fn send_tx() {
-        todo!();
-        // let commitment = U256::from(commitment.to_be_bytes());
-        // let mut tx = self.semaphore.add_member(group_id.into(), commitment);
-        // let pending_tx = if self.eip1559 {
-        // self.provider.fill_transaction(&mut tx.tx, None).await?;
-        // tx.tx.set_gas(10_000_000_u64); // HACK: ethers-rs estimate is wrong.
-        // tx.tx.set_nonce(nonce);
-        // info!(?tx, "Sending transaction");
-        // self.provider.send_transaction(tx.tx, None).await?
-        // } else {
-        // Our tests use ganache which doesn't support EIP-1559 transactions
-        // yet. tx = tx.legacy();
-        // self.provider.fill_transaction(&mut tx.tx, None).await?;
-        // tx.tx.set_nonce(nonce);
-        //
-        // quick hack to ensure tx is so overpriced that it won't get dropped
-        // tx.tx.set_gas_price(
-        // tx.tx
-        // .gas_price()
-        // .ok_or(eyre!("no gasPrice set"))?
-        // .checked_mul(2_u64.into())
-        // .ok_or(eyre!("overflow in gasPrice"))?,
-        // );
-        // info!(?tx, "Sending transaction");
-        // self.provider.send_transaction(tx.tx, None).await?
-        // };
-        // let receipt = pending_tx
-        // .await
-        // .map_err(|e| eyre!(e))?
-        // .ok_or_else(|| eyre!("tx dropped from mempool"))?;
-        // info!(?receipt, "Receipt");
-        // if receipt.status != Some(U64::from(1_u64)) {
-        // return Err(eyre!("tx failed"));
-        // }
     }
 }

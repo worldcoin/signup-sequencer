c71bdb556129424720227f356fe9f90140920620
Remco Bloemen <remco@wicked.ventures>
2022-05-27 15:50:26 -0700

    One group id

--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -21,6 +21,10 @@ pub struct Options {
     #[structopt(long, env, default_value = "174ee9b5fBb5Eb68B6C61032946486dD9c2Dc4b6")]
     pub semaphore_address: Address,
 
+    /// The Semaphore group id to use
+    #[structopt(long, env, default_value = "1")]
+    pub group_id: U256,
+
     /// Mock mode: do not actually submit transactions.
     #[structopt(
         short,
@@ -32,24 +36,32 @@ pub struct Options {
 }
 
 pub struct Contracts {
-    ethereum:  Ethereum,
-    semaphore: Semaphore<ProviderStack>,
-    mock:      bool,
+    ethereum:   Ethereum,
+    semaphore:  Semaphore<ProviderStack>,
+    group_id:   U256,
+    tree_depth: usize,
+    mock:       bool,
 }
 
 impl Contracts {
     #[instrument(level = "debug", skip_all)]
     pub async fn new(options: Options, ethereum: Ethereum) -> EyreResult<Self> {
-        let address = options.semaphore_address;
+        // Sanity check the group id
+        if options.group_id == U256::zero() {
+            error!(group_id = ?options.group_id, "Invalid group id: must be greater than zero");
+            return Err(eyre!("group id must be non-zero"));
+        }
 
         // Sanity check the address
         // TODO: Check that the contract is actually a Semaphore by matching bytecode.
+        let address = options.semaphore_address;
         let code = ethereum.provider().get_code(address, None).await?;
         if code.as_ref().is_empty() {
             error!(
                 ?address,
                 "No contract code deployed at provided Semaphore address"
             );
+            return Err(eyre!("Invalid Semaphore address"));
         }
 
         // Connect to Contract
@@ -59,14 +71,35 @@ impl Contracts {
         let manager = semaphore.manager().call().await?;
         if manager != ethereum.address() {
             error!(?manager, signer = ?ethereum.address(), "Signer is not the manager of the Semaphore contract");
+            return Err(eyre!("Signer is not manager"));
         }
         info!(?address, ?manager, "Connected to Semaphore contract");
+
+        // Make sure the group exists.
+        let tree_depth = semaphore.get_depth(options.group_id).call().await?;
+        if tree_depth == 0 {
+            error!(group_id = ?options.group_id, "Group does not exist");
+            return Err(eyre!("Group does not exist"));
+        }
+        info!(?tree_depth, "Semaphore group found.");
+
+        // TODO: Create group if not exists and flag is set.
 
         Ok(Self {
             ethereum,
             semaphore,
+            group_id: options.group_id,
+            tree_depth: usize::from(tree_depth),
             mock: options.mock,
         })
+    }
+
+    pub fn group_id(&self) -> U256 {
+        self.group_id
+    }
+
+    pub fn tree_depth(&self) -> usize {
+        self.tree_depth
     }
 
     // TODO: Remove this function
@@ -96,6 +129,8 @@ impl Contracts {
         info!(starting_block, "Reading MemberAdded events from chains");
         // TODO: Register to the event stream and track it going forward.
 
+        // TODO: Filter by group id.
+
         // Fetch MemberAdded log events
         let filter = self
             .semaphore
@@ -110,6 +145,7 @@ impl Contracts {
         let mut index = last_leaf;
         let insertions = events
             .iter()
+            .filter(|event| event.group_id == self.group_id)
             .map(|event| {
                 let mut id_bytes = [0u8; 32];
                 event.identity_commitment.to_big_endian(&mut id_bytes);
@@ -151,36 +187,17 @@ impl Contracts {
     }
 
     #[instrument(level = "debug", skip_all)]
-    pub async fn insert_identity(
-        &self,
-        group_id: usize,
-        commitment: &Field,
-        _tree_depth: usize,
-        nonce: usize,
-    ) -> EyreResult<()> {
-        info!(%group_id, %commitment, "Inserting identity in contract");
+    pub async fn insert_identity(&self, commitment: &Field, nonce: usize) -> EyreResult<()> {
+        info!(%commitment, "Inserting identity in contract");
         if self.mock {
             info!(%commitment, "MOCK mode enabled, skipping");
             return Ok(());
         }
 
-        // Check group existence
-        let depth = self
-            .semaphore
-            .get_depth(group_id.into())
-            .from(self.ethereum.address())
-            .call()
-            .await?;
-
-        info!(?group_id, ?depth, "Fetched group tree depth");
-        if depth == 0 {
-            return Err(eyre!("group {} not created", group_id));
-        }
-
         // Send create tx
         let commitment = U256::from(commitment.to_be_bytes());
         self.ethereum
-            .send_transaction(self.semaphore.add_member(group_id.into(), commitment).tx)
+            .send_transaction(self.semaphore.add_member(self.group_id, commitment).tx)
             .await?;
         Ok(())
     }

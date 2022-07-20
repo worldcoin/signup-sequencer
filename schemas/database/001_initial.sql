41aace648ba1154ad7e2d1b63f78c0912760923c
corydickson <corydickson@protonmail.com>
2022-07-20 16:19:51 -0400

    Update schema for pending commitments to be inserted

--- schemas/database/001_initial.sql
+++ schemas/database/001_initial.sql
@@ -8,3 +8,10 @@ CREATE TABLE transactions (
     raw         BYTEA                    NOT NULL,
     hash        BYTEA                        NULL
 );
+
+CREATE TABLE pending_commitments (
+    idx         INT8      PRIMARY KEY    NOT NULL,
+    commitment  BYTEA                    NOT NULL,
+    tx_hash     BYTEA                    NULL,
+    tx_time     TIMESTAMP                NULL
+);

bb9f03ab179974c851babc4b006df0c27e0ce9fb
Grzegorz Swirski <grzegorz@swirski.name>
2022-11-17 14:54:12 +0100

    factor out retry logic

--- src/contracts/caching_log_query.rs
+++ src/contracts/caching_log_query.rs
@@ -85,54 +85,36 @@ impl CachingLogQuery {
                 yield serde_json::from_str(&log).map_err(Error::Parse)?;
             }
 
-            let mut last_successful_block = U64::default();
-            let mut current_page_size = self.page_size;
-            let mut backoff_time = Duration::from_secs(1);
+            let mut retry_stats = RetryStats::new(self.page_size);
 
             'restart: loop {
                 let filter = self.filter.clone().from_block(max(
-                    max(last_successful_block + 1, last_block.db + 1),
+                    max(retry_stats.last_block + 1, last_block.db + 1),
                     self.filter.get_from_block().unwrap_or_default(),
                 ));
 
                 info!(
-                    page_size = current_page_size,
+                    page_size = retry_stats.page_size,
                     from_block = filter.get_from_block().unwrap_or_default().as_u64(),
                     "Reading MemberAdded events from chains"
                 );
 
                 let mut stream = self.provider
-                    .get_logs_paginated(&filter, current_page_size);
+                    .get_logs_paginated(&filter, retry_stats.page_size);
 
                 while let Some(log) = stream.next().await {
-                    let log = match log {
-                        Err(e) if e.to_string().contains("Query timeout exceeded") => {
-                            if current_page_size >= 2000 {
-                                error!(error = ?e, "Retriable error, decreasing page size");
-                                current_page_size /= 2;
-                                continue 'restart;
-                            } else if backoff_time <= Duration::from_secs(32) {
-                                error!(error = ?e, "Retriable error, backoff");
-                                sleep(backoff_time).await;
-                                backoff_time *= 2;
-                                continue 'restart;
-                            } else {
-                                error!(error = ?e, "Retriable error, max number of attempts reached");
-                                // This seems to be the only way to return errors in the try_stream! macro
-                                Err(Error::LoadLogs(e))?;
-                                break 'restart;
-                            }
-                        }
-                        Err(e) => {
+                    let log = match self.handle_retriable_err(log, &mut retry_stats).await {
+                        RetryStatus::Ok(log) => log,
+                        RetryStatus::Restart => continue 'restart,
+                        RetryStatus::Err(e) => {
                             // This seems to be the only way to return errors in the try_stream! macro
-                            Err(Error::LoadLogs(e))?;
+                            Err(e)?;
                             break 'restart;
                         }
-                        Ok(log) => log
                     };
 
                     // If we need to decrease page size later, don't process same blocks twice
-                    last_successful_block = log.block_number.unwrap_or(last_successful_block);
+                    retry_stats.update_last_block(log.block_number);
 
                     if self.is_confirmed(&log, last_block) {
                         let raw_log = serde_json::to_string(&log).map_err(Error::Serialize)?;
@@ -145,6 +127,30 @@ impl CachingLogQuery {
                 // We've iterated over all events, we can kill the restart loop
                 break;
             }
+        }
+    }
+
+    async fn handle_retriable_err(&self, log: Result<Log, LogQueryError<ProviderError>>, stats: &mut RetryStats) -> RetryStatus<Log, Error<ProviderError>> {
+        match log {
+            Err(e) if e.to_string().contains("Query timeout exceeded") => {
+                if stats.page_size >= 2000 {
+                    error!(error = ?e, "Retriable error, decreasing page size");
+                    stats.page_size /= 2;
+                    RetryStatus::Restart
+                } else if stats.backoff_time <= Duration::from_secs(32) {
+                    error!(error = ?e, "Retriable error, backoff");
+                    sleep(stats.backoff_time).await;
+                    stats.backoff_time *= 2;
+                    RetryStatus::Restart
+                } else {
+                    error!(error = ?e, "Retriable error, max number of attempts reached");
+                    RetryStatus::Err(Error::LoadLogs(e))
+                }
+            }
+            Err(e) => {
+                RetryStatus::Err(Error::LoadLogs(e))
+            }
+            Ok(log) => RetryStatus::Ok(log)
         }
     }
 
@@ -212,4 +218,26 @@ impl CachingLogQuery {
 struct LastBlock {
     eth: U64,
     db:  U64,
+}
+
+struct RetryStats {
+    last_block: U64,
+    page_size: u64,
+    backoff_time: Duration
+}
+
+impl RetryStats {
+    fn new(page_size: u64) -> Self {
+        Self { last_block: Default::default(), page_size, backoff_time: Duration::from_secs(1) }
+    }
+
+    fn update_last_block(&mut self, last_block: Option<U64>) {
+        self.last_block = last_block.unwrap_or(self.last_block);
+    }
+}
+
+enum RetryStatus<T, E> {
+    Ok(T),
+    Restart,
+    Err(E),
 }

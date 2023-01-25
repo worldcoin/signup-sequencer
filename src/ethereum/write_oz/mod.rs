8492a9970c63fde251fa2d13a93d283cc0221b74
Grzegorz Swirski <grzegorz@swirski.name>
2023-01-25 13:46:19 +0100

    wip

--- src/ethereum/write_oz/mod.rs
+++ src/ethereum/write_oz/mod.rs
@@ -1,1 +1,29 @@
 
+use clap::Parser;
+use ethers::types::H160;
+
+// TODO: Log and metrics for signer / nonces.
+#[derive(Clone, Debug, PartialEq, Parser)]
+#[group(skip)]
+pub struct Options {
+   /// OpenZeppelin Defender API Key
+   #[clap(long, env)]
+   pub oz_api_key: String,
+
+   /// OpenZeppelin Defender API Secret
+   #[clap(long, env)]
+   pub oz_api_secret: String,
+
+   /// OpenZeppelin Defender API Secret
+   #[clap(long, env, default_value="0x30dcc24131223d4f8af69226e7b11b83e6a68b8b")]
+   pub oz_address: H160, 
+}
+
+#[derive(Clone, Debug)]
+pub struct Provider {
+    inner:        Arc<InnerProvider>,
+    address:      Address,
+    legacy:       bool,
+    send_timeout: Duration,
+    mine_timeout: Duration,
+}

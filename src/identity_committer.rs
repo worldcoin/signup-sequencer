89380823e25b28b16ea74f4dfa646c5a107a5d11
Dzejkop <jakubtrad@gmail.com>
2023-03-09 12:37:20 +0100

    Post merge fix

--- src/identity_committer.rs
+++ src/identity_committer.rs
@@ -76,7 +76,8 @@ impl RunningInstance {
         // which is impossible, since this is the only use, and this method takes
         // ownership, or the channel is closed, which means the committer thread is
         // already dead.
-        let _ = self.shutdown_sender.send(()).await;
+        let _ = self.shutdown_sender.send(());
+
         info!("Awaiting committer shutdown.");
         self.process_identities_handle.await?;
 

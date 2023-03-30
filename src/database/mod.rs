f18f843e9f314c41a2a05195cdcfe8698c54361d
Dzejkop <jakubtrad@gmail.com>
2023-03-30 15:41:59 +0200

    Increase test time tolerance

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -361,7 +361,7 @@ mod test {
         };
 
         ($a:expr, $b:expr) => {
-            assert_same_time!($a, $b, chrono::Duration::milliseconds(10));
+            assert_same_time!($a, $b, chrono::Duration::milliseconds(50));
         };
     }
 

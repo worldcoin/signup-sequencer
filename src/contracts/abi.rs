a34685b7f6e1d558af6c2a701ffcee0cd8ea0d43
Ara Adkins <me@ara.io>
2023-03-31 13:56:00 -0600

    Sync the contract ABI

--- src/contracts/abi.rs
+++ src/contracts/abi.rs
@@ -6,7 +6,6 @@ abigen!(
     BatchingContract,
     r#"[
         struct RootInfo { uint256 root; uint128 supersededTimestamp; bool isValid }
-        struct IdentityUpdate { uint32 leafIndex; uint256 oldCommitment; uint256 newCommitment; }
         function NO_SUCH_ROOT() public pure returns (RootInfo memory rootInfo)
         error UnreducedElement(uint8 elementType, uint256 element)
         error Unauthorized(address user)
@@ -17,13 +16,13 @@ abigen!(
         error NonExistentRoot()
         error ImplementationNotInitalized()
         error NoSuchVerifier()
+        error MismatchedInputLengths()
         constructor(address _logic, bytes memory data) payable
         function initialize(uint8 treeDepth, uint256 initialRoot, address _batchInsertionVerifiers, address _batchUpdateVerifiers, address _semaphoreVerifier, bool _enableStateBridge, address stateBridge) public virtual
         function registerIdentities(uint256[8] calldata insertionProof, uint256 preRoot, uint32 startIndex, uint256[] calldata identityCommitments, uint256 postRoot) public virtual
-        function removeIdentities(uint256[8] calldata removalProof, uint256 preRoot, IdentityUpdate[] calldata removedIdentities, uint256 postRoot) public virtual
-        function updateIdentities(uint256[8] calldata updateProof, uint256 preRoot, IdentityUpdate[] calldata updatedIdentities, uint256 postRoot) public virtual
+        function updateIdentities(uint256[8] calldata updateProof, uint256 preRoot, uint32[] calldata leafIndices, uint256[] calldata oldIdentities, uint256[] calldata newIdentities, uint256 postRoot) public virtual
         function calculateIdentityRegistrationInputHash(uint32 startIndex, uint256 preRoot, uint256 postRoot, uint256[] identityCommitments) public view virtual returns (bytes32 hash)
-        function calculateIdentityUpdateInputHash(uint256 preRoot, uint256 postRoot, IdentityUpdate[] calldata identities) public view virtual returns (bytes32 hash)
+        function calculateIdentityUpdateInputHash(uint256 preRoot, uint256 postRoot, uint32[] calldata leafIndices, uint256[] calldata oldIdentities, uint256[] calldata newIdentities) public view virtual returns (bytes32 hash)
         function latestRoot() public view virtual returns (uint256 root)
         function queryRoot(uint256 root) public view virtual returns (RootInfo memory rootInfo)
         function isInputInReducedForm(uint256 input) public view virtual returns (bool isInReducedForm)

2943e50c0763439b31a677bb8e334de1965f6c90
Dzejkop <jakubtrad@gmail.com>
2023-04-06 10:23:29 +0200

    Handle tx submissions in parallel

--- src/task_monitor.rs
+++ src/task_monitor.rs
@@ -87,6 +87,12 @@ pub struct Options {
     /// space for the insertion.
     #[clap(long, env, default_value = "100")]
     pub insert_identities_capacity: usize,
+
+    /// How many transactions can be held in the pending identities queue
+    /// this translates to how many blockchain transactions we can submit at a
+    /// given time.
+    #[clap(long, env, default_value = "10")]
+    pub pending_identities_capacity: usize,
 }
 
 /// A worker that commits identities to the blockchain.
@@ -100,12 +106,13 @@ pub struct TaskMonitor {
     /// when shutdown is called we want to be able to gracefully
     /// await the join handles - which requires ownership of the handle and by
     /// extension the instance.
-    instance:                   RwLock<Option<RunningInstance>>,
-    database:                   Arc<Database>,
-    identity_manager:           SharedIdentityManager,
-    tree_state:                 TreeState,
-    batch_insert_timeout_secs:  u64,
-    insert_identities_capacity: usize,
+    instance:                    RwLock<Option<RunningInstance>>,
+    database:                    Arc<Database>,
+    identity_manager:            SharedIdentityManager,
+    tree_state:                  TreeState,
+    batch_insert_timeout_secs:   u64,
+    insert_identities_capacity:  usize,
+    pending_identities_capacity: usize,
 }
 
 impl TaskMonitor {
@@ -117,6 +124,8 @@ impl TaskMonitor {
     ) -> Self {
         let batch_insert_timeout_secs = options.batch_timeout_seconds;
         let insert_identities_capacity = options.insert_identities_capacity;
+        let pending_identities_capacity = options.pending_identities_capacity;
+
         Self {
             instance: RwLock::new(None),
             database,
@@ -124,6 +133,7 @@ impl TaskMonitor {
             tree_state,
             batch_insert_timeout_secs,
             insert_identities_capacity,
+            pending_identities_capacity,
         }
     }
 
@@ -137,7 +147,8 @@ impl TaskMonitor {
         // We could use the second element of the tuple as `mut shutdown_receiver`,
         // but for symmetry's sake we create it for every task with `.subscribe()`
         let (shutdown_sender, _) = broadcast::channel(1);
-        let (pending_identities_sender, pending_identities_receiver) = mpsc::channel(1);
+        let (pending_identities_sender, pending_identities_receiver) =
+            mpsc::channel(self.pending_identities_capacity);
         let (insert_identities_sender, insert_identities_receiver) =
             mpsc::channel(self.insert_identities_capacity);
 

8b03b3adbdd38766b32da669f94c8b7ec4815e6e
TheMenko <themenkoprojects@gmail.com>
2023-05-09 12:14:46 +0200

    fixing sql migration syntax errors

--- schemas/database/002_batch_sizes.sql
+++ schemas/database/002_batch_sizes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE provers (
-    batch_size: BIGINT NOT NULL PRIMARY KEY,
-    url: VARCHAR(1028) NOT NULL UNIQUE,
-    timeout_s: BIGINT,
+    batch_size BIGINT NOT NULL PRIMARY KEY,
+    url VARCHAR(1028) NOT NULL UNIQUE,
+    timeout_s BIGINT
 )

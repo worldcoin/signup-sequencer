bd94a83409a2ab95968ab00495f704889499f101
Jakub Trąd <jakubtrad@gmail.com>
2023-05-10 16:02:14 +0200

    Batching/axum (#505)

--- src/server.rs
+++ src/server.rs
@@ -4,31 +4,32 @@ use std::{
     time::Duration,
 };
 
-use ::prometheus::{opts, register_counter, register_histogram, Counter, Histogram};
-use anyhow::{bail, ensure, Context, Error as EyreError, Result as AnyhowResult};
+use anyhow::{bail, ensure, Error as EyreError, Result as AnyhowResult};
+use axum::{extract::State, middleware, response::IntoResponse, routing::post, Json, Router};
 use clap::Parser;
-use cli_batteries::{await_shutdown, trace_from_headers};
-use futures::Future;
-use hyper::{
-    body::Buf,
-    header,
-    service::{make_service_fn, service_fn},
-    Body, Method, Request, Response, Server, StatusCode,
-};
-use once_cell::sync::Lazy;
-use prometheus::{register_int_counter_vec, IntCounterVec};
+use cli_batteries::await_shutdown;
+use hyper::StatusCode;
 use semaphore::{protocol::Proof, Field};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
-use tokio::time::timeout;
-use tracing::{error, info, instrument, trace};
+use tower_http::trace::TraceLayer;
+use tracing::{error, instrument};
 use url::{Host, Url};
 
-use crate::{app::App, database, identity_tree::Hash};
+use crate::{
+    app::{App, InclusionProofResponse, VerifySemaphoreProofResponse},
+    database,
+    identity_tree::Hash,
+};
+
+mod api_metrics_layer;
+mod extract_trace_layer;
+mod timeout_layer;
 
 #[derive(Clone, Debug, PartialEq, Eq, Parser)]
 #[group(skip)]
 pub struct Options {
+    // TODO: This should be a `SocketAddr`. It makes no sense for us to allow a full on URL here
     /// API Server url
     #[clap(long, env, default_value = "http://127.0.0.1:8080/")]
     pub server: Url,
@@ -37,21 +38,6 @@ pub struct Options {
     #[clap(long, env, default_value = "300")]
     pub serve_timeout: u64,
 }
-
-static REQUESTS: Lazy<Counter> =
-    Lazy::new(|| register_counter!(opts!("api_requests", "Number of requests received.")).unwrap());
-static STATUS: Lazy<IntCounterVec> = Lazy::new(|| {
-    register_int_counter_vec!(
-        "api_response_status",
-        "The API responses by status code.",
-        &["status_code"]
-    )
-    .unwrap()
-});
-static LATENCY: Lazy<Histogram> = Lazy::new(|| {
-    register_histogram!("api_latency_seconds", "The API latency in seconds.").unwrap()
-});
-const CONTENT_JSON: &str = "application/json";
 
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -135,105 +121,69 @@ pub enum Error {
 }
 
 impl Error {
-    #[allow(clippy::enum_glob_use)]
-    fn to_response(&self) -> hyper::Response<Body> {
-        use Error::*;
-
-        let status_code = match self {
-            InvalidMethod => StatusCode::METHOD_NOT_ALLOWED,
-            InvalidPath => StatusCode::NOT_FOUND,
-            InvalidContentType => StatusCode::UNSUPPORTED_MEDIA_TYPE,
-            IndexOutOfBounds
-            | IdentityCommitmentNotFound
-            | InvalidCommitment
-            | InvalidSerialization(_) => StatusCode::BAD_REQUEST,
-            DuplicateCommitment => StatusCode::CONFLICT,
+    fn to_status_code(&self) -> StatusCode {
+        match self {
+            Self::InvalidMethod => StatusCode::METHOD_NOT_ALLOWED,
+            Self::InvalidPath => StatusCode::NOT_FOUND,
+            Self::InvalidContentType => StatusCode::UNSUPPORTED_MEDIA_TYPE,
+            Self::IndexOutOfBounds
+            | Self::IdentityCommitmentNotFound
+            | Self::InvalidCommitment
+            | Self::InvalidSerialization(_) => StatusCode::BAD_REQUEST,
+            Self::DuplicateCommitment => StatusCode::CONFLICT,
             _ => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        let status_code = self.to_status_code();
+
+        let body = if let Self::Other(err) = self {
+            format!("{err:?}")
+        } else {
+            self.to_string()
         };
-        hyper::Response::builder()
-            .status(status_code)
-            .body(hyper::Body::from(self.to_string()))
-            .expect("Failed to convert error string into hyper::Body")
+
+        (status_code, body).into_response()
     }
 }
 
-/// Parse a [`Request<Body>`] as JSON using Serde and handle using the provided
-/// method.
-async fn json_middleware<F, T, S, U>(
-    request: Request<Body>,
-    mut next: F,
-) -> Result<Response<Body>, Error>
-where
-    T: DeserializeOwned + Send,
-    F: FnMut(T) -> S + Send,
-    S: Future<Output = Result<U, Error>> + Send,
-    U: Serialize + ToResponseCode,
-{
-    let valid_content_type = request
-        .headers()
-        .get(header::CONTENT_TYPE)
-        .map_or(false, |content_type| content_type == CONTENT_JSON);
-    if !valid_content_type {
-        return Err(Error::InvalidContentType);
-    }
-    let body = hyper::body::aggregate(request).await?;
-    let request = serde_json::from_reader(body.reader())?;
-    let response = next(request).await?;
-    let json = serde_json::to_string_pretty(&response)?;
-    let response = Response::builder()
-        .status(response.to_response_code())
-        .header(header::CONTENT_TYPE, CONTENT_JSON)
-        // No need to include cache-control since POST is not cached by default.
-        .body(Body::from(json))
-        .map_err(Error::Http)?;
-    Ok(response)
+#[instrument(level = "info", skip_all)]
+async fn inclusion_proof(
+    State(app): State<Arc<App>>,
+    Json(inclusion_proof_request): Json<InclusionProofRequest>,
+) -> Result<Json<InclusionProofResponse>, Error> {
+    let result = app
+        .inclusion_proof(&inclusion_proof_request.identity_commitment)
+        .await?;
+
+    Ok(Json(result))
 }
 
-#[instrument(level="info", name="api_request", skip(app), fields(http.uri=%request.uri(), http.method=%request.method()))]
-async fn route(request: Request<Body>, app: Arc<App>) -> Result<Response<Body>, hyper::Error> {
-    trace_from_headers(request.headers());
+#[instrument(level = "info", skip_all)]
+async fn insert_identity(
+    State(app): State<Arc<App>>,
+    Json(insert_identity_request): Json<InsertCommitmentRequest>,
+) -> Result<Json<InclusionProofResponse>, Error> {
+    let result = app
+        .insert_identity(insert_identity_request.identity_commitment)
+        .await?;
 
-    // Measure and log request
-    let _timer = LATENCY.start_timer(); // Observes on drop
-    REQUESTS.inc();
-    trace!(url = %request.uri(), "Receiving request");
+    Ok(Json(result))
+}
 
-    // Route requests
-    let result = match (request.method(), request.uri().path()) {
-        (&Method::POST, "/verifySemaphoreProof") => {
-            json_middleware(request, |request: VerifySemaphoreProofRequest| {
-                let app = app.clone();
-                async move { app.verify_semaphore_proof(&request).await }
-            })
-            .await
-        }
-        (&Method::POST, "/inclusionProof") => {
-            json_middleware(request, |request: InclusionProofRequest| {
-                let app = app.clone();
-                async move { app.inclusion_proof(&request.identity_commitment).await }
-            })
-            .await
-        }
-        (&Method::POST, "/insertIdentity") => {
-            json_middleware(request, |request: InsertCommitmentRequest| {
-                let app = app.clone();
-                async move { app.insert_identity(request.identity_commitment).await }
-            })
-            .await
-        }
-        (&Method::POST, _) => Err(Error::InvalidPath),
-        _ => Err(Error::InvalidMethod),
-    };
-    let response = result.unwrap_or_else(|err| {
-        error!(%err, "Error handling request");
-        err.to_response()
-    });
+#[instrument(level = "info", skip_all)]
+async fn verify_semaphore_proof(
+    State(app): State<Arc<App>>,
+    Json(verify_semaphore_proof_request): Json<VerifySemaphoreProofRequest>,
+) -> Result<Json<VerifySemaphoreProofResponse>, Error> {
+    let result = app
+        .verify_semaphore_proof(&verify_semaphore_proof_request)
+        .await?;
 
-    // Measure result and return
-    STATUS
-        .with_label_values(&[response.status().as_str()])
-        .inc();
-    Ok(response)
+    Ok(Json(result))
 }
 
 /// # Errors
@@ -252,6 +202,7 @@ pub async fn main(app: Arc<App>, options: Options) -> AnyhowResult<()> {
         "Only / is supported in {}",
         options.server
     );
+
     let ip: IpAddr = match options.server.host() {
         Some(Host::Ipv4(ip)) => ip.into(),
         Some(Host::Ipv6(ip)) => ip.into(),
@@ -273,108 +224,30 @@ pub async fn main(app: Arc<App>, options: Options) -> AnyhowResult<()> {
 ///
 /// Will return `Err` if the provided `listener` address cannot be accessed or
 /// if the server fails to bind to the given address.
-///
-/// # Panics
-///
-/// Panics if the request handler exceeds the provided `serve_timeout`.
 pub async fn bind_from_listener(
     app: Arc<App>,
     serve_timeout: Duration,
     listener: TcpListener,
 ) -> AnyhowResult<()> {
-    let local_addr = listener.local_addr()?;
-    let make_svc = make_service_fn(move |_| {
-        // Clone here as `make_service_fn` is called for every connection
-        let app = app.clone();
-        let serve_timeout = serve_timeout;
+    let router = Router::new()
+        .route("/verifySemaphoreProof", post(verify_semaphore_proof))
+        .route("/inclusionProof", post(inclusion_proof))
+        .route("/insertIdentity", post(insert_identity))
+        .layer(TraceLayer::new_for_http())
+        // Custom layers
+        .layer(middleware::from_fn(api_metrics_layer::middleware))
+        .layer(middleware::from_fn_with_state(
+            serve_timeout,
+            timeout_layer::middleware,
+        ))
+        .layer(middleware::from_fn(extract_trace_layer::middleware))
+        .with_state(app.clone());
 
-        async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| {
-                // Clone here as `service_fn` is called for every request
-                let app = app.clone();
-                let serve_timeout = serve_timeout;
-
-                async move {
-                    timeout(serve_timeout, route(req, app))
-                        .await
-                        .unwrap_or_else(|err| {
-                            error!(?err, timeout = ?serve_timeout, "Timeout while handling request");
-
-                            panic!("Sequencer may be stalled, terminating.")
-                        })
-                }
-            }))
-        }
-    });
-
-    let server = Server::from_tcp(listener)
-        .context("Failed to bind address")?
-        .serve(make_svc)
+    let server = axum::Server::from_tcp(listener)?
+        .serve(router.into_make_service())
         .with_graceful_shutdown(await_shutdown());
 
-    info!(url = %local_addr, "Server listening");
-
     server.await?;
+
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use hyper::{Request, StatusCode};
-    use serde_json::json;
-
-    use super::*;
-
-    // TODO: Fix test
-    // #[tokio::test]
-    async fn _test_inclusion_proof() {
-        let options = crate::app::Options::try_parse_from([""]).unwrap();
-        let app = Arc::new(App::new(options).await.unwrap());
-        let body = Body::from(
-            json!({
-                "identityIndex": 0,
-            })
-            .to_string(),
-        );
-        let request = Request::builder()
-            .method("POST")
-            .uri("/inclusionProof")
-            .header("Content-Type", "application/json")
-            .body(body)
-            .unwrap();
-        let res = route(request, app).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK);
-        // TODO deserialize proof and compare results
-    }
-}
-
-#[cfg(feature = "bench")]
-#[allow(unused_imports)]
-#[doc(hidden)]
-pub mod bench {
-    use criterion::{black_box, Criterion};
-    use hyper::body::to_bytes;
-
-    use super::*;
-    use crate::bench::runtime;
-
-    pub fn group(_c: &mut Criterion) {
-        //     bench_hello_world(c);
-    }
-
-    // fn bench_hello_world(c: &mut Criterion) {
-    //     let app = Arc::new(App::new(2));
-    //     let request = CommitmentRequest {
-    //         identity_commitment:
-    // "24C94355810D659EEAA9E0B9E21F831493B50574AA2D3205F0AAB779E2864623"
-    //             .to_string(),
-    //     };
-    //     c.bench_function("bench_insert_identity", |b| {
-    //         b.to_async(runtime()).iter(|| async {
-    //             let response =
-    // app.insert_identity(request.clone()).await.unwrap();             let
-    // bytes = to_bytes(response.into_body()).await.unwrap();
-    // drop(black_box(bytes));         });
-    //     });
-    // }
 }
---
+++ src/server/extract_trace_layer.rs
@@ -0,0 +1,19 @@
+use axum::{
+    http::{Request, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+
+pub async fn middleware<B>(request: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
+    let (parts, body) = request.into_parts();
+
+    cli_batteries::trace_from_headers(&parts.headers);
+
+    let request = Request::from_parts(parts, body);
+
+    let mut response = next.run(request).await;
+
+    cli_batteries::trace_to_headers(response.headers_mut());
+
+    Ok(response)
+}

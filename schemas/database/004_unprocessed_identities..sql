51b87ff79123ae339bb36d287b1cbc6353ddb116
0xKitsune <0xKitsune@protonmail.com>
2023-08-28 01:21:48 -0400

    updated existing tables, added new tables for recoveries, deletions

---
+++ schemas/database/004_unprocessed_identities..sql
@@ -0,0 +1,8 @@
+CREATE TABLE unprocessed_identities (
+    commitment    BYTEA       NOT NULL UNIQUE,
+    status        VARCHAR(50) NOT NULL,
+    created_at    TIMESTAMPTZ NOT NULL,
+    processed_at  TIMESTAMPTZ,
+    error_message TEXT
+    eligibility_timestamp TIMESTAMPTZ
+)

b8e4d5d04a95abd7b064aca5a84a91ba027da016
0xKitsune <0xKitsune@protonmail.com>
2023-08-28 17:59:27 -0400

    updated deletion table, updated insert deletion test

--- schemas/database/006_deletions.sql
+++ schemas/database/006_deletions.sql
@@ -1,4 +1,4 @@
 CREATE TABLE deletions (
-    leaf_index             BIGINT      NOT NULL PRIMARY KEY
-    commitment    BYTEA       NOT NULL UNIQUE,
+    leaf_index    BIGINT      NOT NULL PRIMARY KEY,
+    commitment    BYTEA       NOT NULL UNIQUE
 )

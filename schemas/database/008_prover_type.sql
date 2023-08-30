edd6d7e425a88d1212ad8aa104e4f237a20eb732
0xKitsune <0xKitsune@protonmail.com>
2023-08-29 20:44:07 -0400

    updated prover table migration

--- schemas/database/008_prover_type.sql
+++ schemas/database/008_prover_type.sql
@@ -1,5 +1,11 @@
-ALTER TABLE provers ADD COLUMN prover_type ENUM('Insertion', 'Deletion');
+-- Create ENUM for prover type
+CREATE TYPE prover_enum AS ENUM('Insertion', 'Deletion');
 
+-- Add new column with the enum
+ALTER TABLE provers ADD COLUMN prover_type prover_enum;
+
+-- Update the new column, setting all existing provers as insertions
 UPDATE provers SET prover_type = 'Insertion' WHERE prover_type IS NULL;
 
-ALTER TABLE provers MODIFY COLUMN prover_type ENUM('Insertion', 'Deletion') NOT NULL;
+-- Make the column NOT NULL
+ALTER TABLE provers ALTER COLUMN prover_type SET NOT NULL;

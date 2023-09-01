3fd8a3dc3dca612cb31db75ea88bb4f7cafd8a48
0xKitsune <0xKitsune@protonmail.com>
2023-09-01 18:45:43 -0400

    updated delete task

--- src/task_monitor/tasks/delete_identities.rs
+++ src/task_monitor/tasks/delete_identities.rs
@@ -1,11 +1,11 @@
 use std::collections::HashSet;
 use std::sync::Arc;
-use std::time::{Duration, SystemTime};
 
 use anyhow::Result as AnyhowResult;
 use ethers::types::U256;
+use tokio::select;
 use tokio::sync::Notify;
-use tokio::{select, time};
+use tokio::time::{Duration, Instant};
 use tracing::{info, instrument};
 
 use crate::contracts::{IdentityManager, SharedIdentityManager};
@@ -20,11 +20,11 @@ use crate::utils::async_queue::{AsyncPopGuard, AsyncQueue};
 // It can take up to 40 minutes to bridge the root
 
 pub struct DeleteIdentities {
-    database:         Arc<Database>,
-    identity_manager: SharedIdentityManager,
-    latest_tree:      TreeVersion<Latest>,
-
-    wake_up_notify: Arc<Notify>,
+    database:                Arc<Database>,
+    identity_manager:        SharedIdentityManager,
+    latest_tree:             TreeVersion<Latest>,
+    deletion_time_interval:  Duration,
+    min_deletion_batch_size: usize,
 }
 
 impl DeleteIdentities {
@@ -32,14 +32,15 @@ impl DeleteIdentities {
         database: Arc<Database>,
         identity_manager: SharedIdentityManager,
         latest_tree: TreeVersion<Latest>,
-
-        wake_up_notify: Arc<Notify>,
+        deletion_time_interval: Duration,
+        min_deletion_batch_size: usize,
     ) -> Arc<Self> {
         Arc::new(Self {
             database,
             identity_manager,
             latest_tree,
-            wake_up_notify,
+            deletion_time_interval,
+            min_deletion_batch_size,
         })
     }
 
@@ -47,7 +48,9 @@ impl DeleteIdentities {
         delete_identities(
             &self.database,
             &self.identity_manager,
-            &self.latest_tree & self.wake_up_notify,
+            &self.latest_tree,
+            self.deletion_time_interval,
+            self.min_deletion_batch_size,
         )
         .await
     }
@@ -57,98 +60,78 @@ impl DeleteIdentities {
 async fn delete_identities(
     database: &Database,
     identity_manager: &IdentityManager,
-    wake_up_notify: &Notify,
-    latest_tree: TreeVersion<Latest>,
-    deletion_sleep_time: Duration,
+    latest_tree: &TreeVersion<Latest>,
+    deletion_time_interval: Duration,
+    min_deletion_batch_size: usize,
 ) -> AnyhowResult<()> {
-    // TODO: do we need to do this?
+    // TODO: do we need to do this for this step?
     info!("Awaiting for a clean slate");
     identity_manager.await_clean_slate().await?;
 
-    info!("Starting identity processor.");
+    info!("Starting deletion processor.");
 
-    // TODO: should this be max batch size for deletions
-    let batch_size = identity_manager.max_batch_size().await;
-
-    // We start a timer and force it to perform one initial tick to avoid an
-    // immediate trigger.
-    let mut timer = time::interval(deletion_sleep_time);
-    timer.tick().await;
-
-    // When both futures are woken at once, the choice is made
-    // non-deterministically. This could, in the worst case, result in users waiting
-    // for twice `timeout_secs` for their insertion to be processed.
-    //
-    // To ensure that this does not happen we track the last time a batch was
-    // inserted. If we have an incomplete batch but are within a small delta of the
-    // tick happening anyway in the wake branch, we insert the current
-    // (possibly-incomplete) batch anyway.
-    let mut last_batch_time: SystemTime = SystemTime::now();
-
-    // TODO: we need to track last deletion and update this, making sure that there
-    // is a deletion every x interval
-
+    let mut last_deletion = Instant::now();
     loop {
         let deletions = database.get_deletions().await?;
         if deletions.is_empty() {
-            tokio::time::sleep(Duration::from_secs(5)).await;
+            // Sleep for one hour
+            tokio::time::sleep(Duration::from_secs(3600)).await;
             continue;
         }
 
-        // TODO: check batch size or time elapsed since last deletion
+        // If the minimum deletions batch size is reached or the deletion time interval
+        // has elapsed, run a batch of deletions
+        if deletions.len() >= min_deletion_batch_size
+            || Instant::now() - last_deletion > deletion_time_interval
+        {
+            // Dedup deletion entries
+            let deletions = deletions
+                .into_iter()
+                .map(|f| f)
+                .collect::<HashSet<DeletionEntry>>();
 
-        // Dedup deletion entries
-        let deletions = deletions
-            .into_iter()
-            .map(|f| f)
-            .collect::<HashSet<DeletionEntry>>();
+            // Ensure that the next leaf matches in the db and the latest tree
+            let next_db_index = database.get_next_leaf_index().await?;
+            let next_leaf = latest_tree.next_leaf();
 
-        // Validate the identities are not in the database
+            assert_eq!(
+                next_leaf, next_db_index,
+                "Database and tree are out of sync. Next leaf index in tree is: {next_leaf}, in \
+                 database: {next_db_index}"
+            );
 
-        let next_db_index = database.get_next_leaf_index().await?;
-        let next_leaf = latest_tree.next_leaf();
+            let (leaf_indices, previous_commitments): (Vec<usize>, Vec<Hash>) = deletions
+                .iter()
+                .map(|d| (d.leaf_index, d.commitment))
+                .unzip();
 
-        assert_eq!(
-            next_leaf, next_db_index,
-            "Database and tree are out of sync. Next leaf index in tree is: {next_leaf}, in \
-             database: {next_db_index}"
-        );
+            // Delete the commitments at the target leaf indices in the latest tree,
+            // generating the proof for each update
+            let data = latest_tree.delete_many(&leaf_indices);
 
-        let (leaf_indices, previous_commitments) = deletions
-            .iter()
-            .map(|d| (d.leaf_index, d.commitment))
-            .unzip();
+            assert_eq!(
+                data.len(),
+                leaf_indices.len(),
+                "Length mismatch when appending identities to tree"
+            );
 
-        let data = latest_tree.delete_many(&leaf_indices);
+            // Insert the new items into pending identities
+            let items = data.into_iter().zip(leaf_indices.into_iter());
+            for ((root, _proof), leaf_index) in items {
+                database
+                    .insert_pending_identity(leaf_index, &Hash::ZERO, &root)
+                    .await?;
+            }
 
-        assert_eq!(
-            data.len(),
-            leaf_indices.len(),
-            "Length mismatch when appending identities to tree"
-        );
+            // Remove the previous commitments from the deletions table
+            database.remove_deletions(previous_commitments).await?;
 
-        let items = data.into_iter().zip(leaf_indices.into_iter());
+            // TODO: unlock mutex
 
-        // TODO: insert into identities
-        for ((root, _proof), leaf_index) in items {
-            database
-                .insert_pending_identity(*leaf_index, &Hash::ZERO, &root)
-                .await?;
+            // TODO: notify to start task to insert on chain
+
+            // Update the last deletion time
+            last_deletion = Instant::now();
         }
-
-        database.remove_deletions(previous_commitments).await?;
-
-        // TODO: remove all deletions, make note if we should do this as a batch
-        // or one by one
-
-        // Lock mutex
-
-        // Get all of the deletions
-
-        // Insert new identities into idt table
-
-        // Update some tree ?
-
-        // notify?
     }
 }

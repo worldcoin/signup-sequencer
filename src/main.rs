ab51a07b405069b6b974b55378ebf60098ff9b65
Dzejkop <jakubtrad@gmail.com>
2024-04-22 21:34:24 +0200

    Log config

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,8 @@ async fn sequencer_app(args: Args) -> anyhow::Result<()> {
 
     watch_shutdown_signals();
 
+    tracing::info!(?config, "Starting the app");
+
     let server_config = config.server.clone();
 
     // Create App struct

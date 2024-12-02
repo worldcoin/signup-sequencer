54278fd523073e319a8e642ad2a6f1656a3de43a
Piotr Heilman <piotr.heilman@gmail.com>
2024-12-02 12:19:37 +0100

    Change monitor task.

--- src/task_monitor/tasks/monitor_queue.rs
+++ src/task_monitor/tasks/monitor_queue.rs
@@ -1,15 +1,20 @@
-use std::sync::Arc;
-
-use tokio::time::{sleep, Duration};
-
 use crate::task_monitor::{App, TaskMonitor};
+use std::sync::Arc;
+use tokio::time;
+use tokio::time::{Duration, MissedTickBehavior};
+use tracing::info;
 
-// How often send metrics for idenity queue length
-const QUEUE_MONITORING_PERIOD: Duration = Duration::from_secs(1);
+// How often send metrics for identity queue length
+const QUEUE_MONITORING_PERIOD: Duration = Duration::from_secs(30);
 
 pub async fn monitor_queue(app: Arc<App>) -> anyhow::Result<()> {
+    let mut timer = time::interval(QUEUE_MONITORING_PERIOD);
+    timer.set_missed_tick_behavior(MissedTickBehavior::Skip);
+
     loop {
+        timer.tick().await;
+        info!("Monitor queue woken due to timeout.");
+
         TaskMonitor::log_identities_queues(&app.database).await?;
-        sleep(QUEUE_MONITORING_PERIOD).await;
     }
 }
